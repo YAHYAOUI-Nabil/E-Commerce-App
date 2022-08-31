2912e850e8e8842d19ab9f9a2abf5a5b13926e82
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2022-09-01 01:20:34 +0200

    refactor: building of the sidebar menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search';
+
+const Container = styled.div`
+    height: 60px;
+`
+const Wrapper = styled.div`
+    display: flex;
+    margin: 10px 10px;
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Center = styled.div`
+    flex: 1;
+`
+const Right = styled.div`
+    flex: 1;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            input
+            <SearchIcon />
+          </SearchContainer>
+        </Left>
+        <Center>center</Center>
+        <Right>right</Right>
+      </Wrapper>
+    </Container>
   )
 }
 
